4e8204893c03157bc2352d277078beb93d4191a1
Kiwi-Cj <johnchristianvaldez23@gmail.com>
2026-02-21 13:48:15 +0800

    a

--- index.php
+++ index.php
@@ -5,13 +5,27 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-if (file_exists($maintenance = __DIR__.'/storage/framework/maintenance.php')) {
+$basePath = __DIR__;
+
+if (! file_exists($basePath.'/vendor/autoload.php') || ! file_exists($basePath.'/bootstrap/app.php')) {
+    $parentPath = dirname(__DIR__);
+    if (file_exists($parentPath.'/vendor/autoload.php') && file_exists($parentPath.'/bootstrap/app.php')) {
+        $basePath = $parentPath;
+    }
+}
+
+if (file_exists($maintenance = $basePath.'/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-require __DIR__.'/vendor/autoload.php';
+if (! file_exists($basePath.'/vendor/autoload.php')) {
+    http_response_code(500);
+    exit('Laravel autoload not found. Check deployment paths and run composer install.');
+}
+
+require $basePath.'/vendor/autoload.php';
 
 /** @var Application $app */
-$app = require_once __DIR__.'/bootstrap/app.php';
+$app = require_once $basePath.'/bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
