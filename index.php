8eed63de511944c367fbc1a66613f6e792bc141c
Kiwi-Cj <johnchristianvaldez23@gmail.com>
2026-02-21 10:29:53 +0800

    S

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
 // Directly include the home page (no redirect)
-require_once DIR . '/resources/views/welcome.blade.php'; 
+require_once DIR . '/Lgu1-energy/resources/views/welcome.blade.php'; 
