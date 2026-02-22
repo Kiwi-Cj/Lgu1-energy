7c5d2265578aae3efb19df6d716affb1487a51b4
Kiwi-Cj <johnchristianvaldez23@gmail.com>
2026-02-22 22:03:23 +0800

    a

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -134,7 +134,11 @@
                 <li class="nav-item"><a class="nav-link" href="#features">Features</a></li>
                 <li class="nav-item"><a class="nav-link" href="#testimonials">Testimonials</a></li>
                 <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
-                <li class="nav-item"><a class="btn btn-primary ms-lg-3" href="{{ url('/login') }}">Login</a></li>
+                @guest
+                    <li class="nav-item"><a class="btn btn-primary ms-lg-3" href="{{ route('login') }}">Login</a></li>
+                @else
+                    <li class="nav-item"><a class="btn btn-primary ms-lg-3" href="{{ route('dashboard') }}">Dashboard</a></li>
+                @endguest
             </ul>
         </div>
     </div>
